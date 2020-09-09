bf373c75c41aad77ee64f2df318d6e8a2f4ebc37
dmitrysitnik <dmitry.sitnik46@gmail.com>
2020-09-09 12:19:14 +0300

    write the new contacts block

--- app/contacts.php
+++ app/contacts.php
@@ -1,18 +1,18 @@
 <div class="w3-content w3-container w3-padding-64" data-aos="fade-up" id="contact">
     <h3 class="w3-center">КОНТАКТЫ</h3>
-    <p class="w3-center"><em>Связаться с нами можно следующи способами:</em></p>
+    <p class="w3-center">
+      <em>Вы можете связаться с нами через форму ниже. 
+          Также по любым вопросам можно написать на указанный e-mail или в наши социальные сети.
+      </em>
+    </p>
 
     <div class="w3-row w3-padding-32 w3-section">
-      <div class="w3-col m4 w3-container">
-        <img src="../app/Photo/test-map.png" class="w3-image w3-round" style="width:100%">
-      </div>
-      <div class="w3-col m8 w3-panel">
+      <div class="w3-col m12 w3-panel w3-center">
         <div class="w3-large w3-margin-bottom">
           <i class="fa fa-map-marker fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> Санкт-Петербург<br>
-          <i class="fa fa-phone fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> Phone: +7 (800) 800-80-80<br>
           <i class="fa fa-envelope fa-fw w3-hover-text-black w3-xlarge w3-margin-right"></i> Email: mail@mail.com<br>
         </div>
-        <p>Вы можете связаться с нами через форму ниже. Также по любым вопросам можно написать на указанный e-mail или в наши социальные сети</p>
+        <p></p>
         <form action="../web/mail.php" target="_blank" method="post">
           <div class="w3-row-padding" style="margin:0 -16px 8px -16px">
             <div class="w3-half">
@@ -29,5 +29,23 @@
         </form>
       </div>
     </div>
+
+    <div class="w3-row w3-padding-32 w3-section" data-aos="fade-up">
+      <div class="w3-content w3-container w3-center">
+        <h3>НАШИ ПРОФИЛИ В СОЦИАЛЬНЫХ СЕТЯХ</h3>
+        <p class="w3-center">
+         <em>
+           Не забудьте подписаться на наши профили в социальных сетях. 
+           Здесь Вы можете узнать о событиях происходящих в команде, турнирах, сборах, а также лучше узнать команду и игроков. 
+         </em>
+        </p>
+        <div class="w3-xlarge w3-center w3-section">
+          <a href="https://vk.com/blackbearspolytech" target="_blank"><i class="fa fa-vk fa-2x w3-hover-opacity"></i></a>
+          <a href="https://www.instagram.com/blackbearspolytech/" target="_blank"><i class="fa fa-instagram fa-2x w3-hover-opacity"></i></a>
+          <a href="https://www.youtube.com/channel/UCo6bFQxmow4rn0-Cmfl6BPA" target="_blank"><i class="fa fa-youtube fa-2x w3-hover-opacity"></i></a>
+        </div>
+      </div>
+    </div>
+
   </div>
 </div>
